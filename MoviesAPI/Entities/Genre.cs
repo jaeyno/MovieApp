827395cc621a49bbde22032539163b9e5ca9b26b
Jae No <jaeyno@gmail.com>
2021-03-17 16:26:08 -0400

    <Added a custom validation, "FirstLetterUppercase">

--- MoviesAPI/Entities/Genre.cs
+++ MoviesAPI/Entities/Genre.cs
@@ -13,6 +13,7 @@ namespace MoviesAPI.Entities
 
         [Required(ErrorMessage = "The field with name {0} is required")]
         [StringLength(10)]
+        [FirstLetterUppercase]
         public string Name { get; set; }
 
         [Range(18, 120)]
