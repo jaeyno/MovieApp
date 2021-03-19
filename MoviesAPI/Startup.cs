39e6173d629b83f3f000eb7c2fd5f0a13d5c0edb
Jae No <jaeyno@gmail.com>
2021-03-19 08:03:43 -0400

    <Entity Framework Setup with MSSQL> 	new file:   MoviesAPI/ApplicationDbContext.cs

--- MoviesAPI/Startup.cs
+++ MoviesAPI/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -29,6 +30,12 @@ namespace MoviesAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {   
+            //Added EntityFramework
+            services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+            });
+
             //Added cors
             services.AddCors(options => 
             {
